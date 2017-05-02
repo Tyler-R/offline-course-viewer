b903edeead600a7ed6462e17fd2d018d9af56dcf
Tyler-R <trubenui@sfu.ca>
2017-05-02 14:16:02 -0700

    Add file that initializes sequelize that other model classes can use

---
+++ app/models/sequelize.js
@@ -0,0 +1,12 @@
+var Sequelize = require('sequelize'),
+    config = require('../../config/database.js');
+
+module.exports.sequelize = new Sequelize(config.database, config.user, config.password, {
+    host: config.host,
+    dialect: 'postgres',
+    pool: {
+        max: 10,
+        min: 0,
+        idle: 10000
+    }
+});
