24de09a1c03e28bcc1b436f306dc36d52f4820ac
Tyler-R <trubenui@sfu.ca>
2017-05-12 13:16:02 -0700

    Add materialize dependency to client.js

--- app/client.js
+++ app/client.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+window.$ = window.jQuery = require('materialize-css/node_modules/jquery/dist/jquery.js');
+require('materialize-css/dist/css/materialize.css');
+require('materialize-css/dist/js/materialize.js');
+require('materialize-css/js/init.js');
+
 import App from "./client/App.js";
 
 ReactDOM.render(
