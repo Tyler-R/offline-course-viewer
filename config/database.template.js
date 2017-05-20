9c55719b4e2e6f601f5944158ef1aef659d86d9b
Tyler Rubenuik <trubenui@sfu.ca>
2017-05-20 11:22:53 -0700

    Fixed typo in 'of'

--- config/database.template.js
+++ config/database.template.js
@@ -3,7 +3,7 @@ module.exports = {
     user: 'username',
     password: 'password',
     database: 'name',
-    // the type fo database that will be used. Currently postgres and sqlite are suppored based on the package.json dependencies.
+    // the type of database that will be used. Currently postgres and sqlite are suppored based on the package.json dependencies.
     dialect: 'postgres',
     // a path to the sqlite path that the database data will be stored in if sqlite is used as the database type.
     sqlitePath: '/home/path/coursera-dl-viewer/database.sqlite',
