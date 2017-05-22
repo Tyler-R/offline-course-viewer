889b70e7835279cdca35ac496f5fbc1f74591a65
Tyler-R <trubenui@sfu.ca>
2017-05-22 12:49:07 -0700

    Add react router to App.js to navigate between pages
    Now the react router allows for rendering of both a home page will all
    the courses on it, and sub pages with individual course information or
    lecture viewing on it.

--- app/client/App.js
+++ app/client/App.js
@@ -1,69 +1,26 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import axios from 'axios';
 
-import Course from './components/course/course.js'
+import Home from './components/home/home.js';
+import LecturePage from './components/lecture-page/lecture-page.js';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            courses: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get('/courses')
-            .then(res => {
-                const courses = res.data
-                this.setState({courses});
-
-                $(document).ready(() => {
-                    // stops button and icons from expanding and collapsing the collapsible section
-                    $(".collapse-disabled").click((e) => {
-                        e.stopPropagation();
-                    });
-
-                    $(".dropdown").click((e) => {
-                        if($(e.target).hasClass('dropdown-icon')) {
-                            // dropdown icon wwas clicked do display dropdown menu
-                            $(e.target).siblings(".dropdown-list").toggleClass("show");
-                        } else {
-                            // item in dropdown menu was clicked
-                            $(e.target).parents('.dropdown-list').toggleClass("show")
-                            console.log($(e.target));
-                        }
-                    });
-
-                    window.onclick = (e) => {
-                        if (!event.target.matches('.dropdown-icon')) {
-
-                            let dropdowns = document.getElementsByClassName("dropdown-list");
-                            for (let i = 0; i < dropdowns.length; i++) {
-                                var openDropdown = dropdowns[i];
-                                if (openDropdown.classList.contains('show')) {
-                                    openDropdown.classList.remove('show');
-                                }
-                            }
-                        }
-                    }
-
-                });
-            });
-    }
-
     render() {
-        let courses = [];
-
-        for(let i = 0; i < this.state.courses.length; i++) {
-            let course = this.state.courses[i];
-            courses.push(<Course key={course} id={i} name={course}></Course>);
-        }
-
         return (
-            <div>
-                {courses}
-            </div>
+            <Router>
+                <div>
+                    <Route exact={true} path='/' component={Home}/>
+                    <Route path="/:course/:week/:module/:lecture" component={LecturePage}/>
+
+                </div>
+
+            </Router>
         );
     }
 }
