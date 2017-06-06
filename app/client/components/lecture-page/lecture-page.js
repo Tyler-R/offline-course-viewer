52f29793813ffdf5a3077a887ab8e58946212c5a
Tyler-R <trubenui@sfu.ca>
2017-06-05 23:51:03 -0700

    Modify link to use new link and add video / reading lectures

--- app/client/components/lecture-page/lecture-page.js
+++ app/client/components/lecture-page/lecture-page.js
@@ -2,26 +2,48 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-import Sidebar from './sidebar/sidebar.js'
+import Sidebar from './sidebar/sidebar.js';
+import VideoLecture from './VideoLecture/VideoLecture.js';
+import ReadingLecture from './readingLecture/readingLecture.js';
+
+import style from './lecturePage.scss';
 
 class LecturePage extends Component {
     constructor(props) {
         super(props);
         let params = props.match.params;
+        console.log(params);
 
         this.state = {
             courseName: params.courseName,
             weekId: params.weekId,
             lectureId: params.lectureId,
             lectureName: params.lectureName,
+            lectureType: params.lectureType
         }
     }
 
     render() {
+        let lecture = undefined;
+
+        if(this.state.lectureType == "video") {
+            lecture = (
+                <VideoLecture
+                    key={this.state.lectureId}
+                    id={this.state.lectureId}
+                    name={this.state.lectureName}
+                />
+            );
+        } else if(this.state.lectureType == "reading") {
+            lecture = (
+                <ReadingLecture />
+            );
+        }
+
         return (
             <span>
-                <Sidebar key={this.state.weekId} weekId={this.state.weekId}/>
-
+                <Sidebar key={this.state.weekId} weekId={this.state.weekId} courseName={this.state.courseName}/>
+                {lecture}
             </span>
         );
     }
