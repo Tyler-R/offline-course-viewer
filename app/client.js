76419611fb7abbde78d5befb446d155803bc8cb7
Tyler-R <trubenui@sfu.ca>
2017-06-21 05:35:47 -0700

    Add Redux store to react app

--- app/client.js
+++ app/client.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 window.$ = window.jQuery = require('materialize-css/node_modules/jquery/dist/jquery.js');
 require('materialize-css/dist/css/materialize.css');
@@ -7,8 +9,13 @@ require('materialize-css/dist/js/materialize.js');
 require('materialize-css/js/init.js');
 
 import App from "./client/App.js";
+import reducers from './client/reducers/index.js';
+
+let store = createStore(reducers);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('container')
 );
