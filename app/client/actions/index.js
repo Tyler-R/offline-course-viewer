b958d3de3da8e3dfe78bbceafb4fd17321d84bc2
Tyler-R <trubenui@sfu.ca>
2017-06-28 14:18:55 -0700

    Add Redux actionn to complete a lecture and select a playlist`

--- app/client/actions/index.js
+++ app/client/actions/index.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export const ADD_LECTURE_GROUPS = "ADD_LECTURE_GROUPS";
+
 export const ADD_LECTURES = "ADD_LECTURES";
 export const TOGGLE_COLLAPSED = "TOGGLE_COLLAPSED";
+export const COMPLETE_LECTURE = "COMPLETE_LECTURE";
+
+export const SELECT_PLAYLIST = "SELECT_PLAYLIST";
 
 export function addLectureGroups(lectureGroups, weekId) {
     return {
@@ -11,6 +15,7 @@ export function addLectureGroups(lectureGroups, weekId) {
         lectureGroups,
     };
 }
+
 
 export function addLectures(lectures, lectureGroupId) {
     return {
@@ -25,5 +30,20 @@ export function toggleCollapsed(collapsed, lectureGroupId) {
         type: TOGGLE_COLLAPSED,
         collapsed,
         lectureGroupId,
+    }
+}
+
+export function completeLecture(lectureId) {
+    return {
+        type: COMPLETE_LECTURE,
+        lectureId,
+    }
+}
+
+
+export function selectPlaylist(playlistId) {
+    return {
+        type: SELECT_PLAYLIST,
+        playlistId,
     }
 }
