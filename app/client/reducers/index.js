0fe4eee4b4e61d59f9f505cdfafee4a6142957f2
Tyler-R <trubenui@sfu.ca>
2017-07-10 03:56:37 -0700

    Add course and playlist reducer to reducer index file

--- app/client/reducers/index.js
+++ app/client/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
 
-import lectureGroupReducer from './lecturePage/lectureGroupReducer.js'
-import lectureReducer from './lecturePage/lectureReducer.js'
+import lectureGroupReducer from './lecturePage/lectureGroupReducer.js';
+import lectureReducer from './lecturePage/lectureReducer.js';
+import playlistReducer from './home/playlistReducer.js';
+import courseReducer from './home/courseReducer.js';
 
 const reducers = combineReducers({
     lectureGroups: lectureGroupReducer,
-    lectures: lectureReducer
+    lectures: lectureReducer,
+    playlists: playlistReducer,
+    courses: courseReducer,
 })
 
 export default reducers;
