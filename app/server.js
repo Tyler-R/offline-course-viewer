7ccbe159a4aa470c85235d226a7ff819c067458f
Tyler-R <trubenui@sfu.ca>
2017-07-21 19:56:45 -0700

    Add server route to swap position of playlists

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 var schema = require('./models/schema.js'),
+    sequelize = require('./models/sequelize.js'),
     bodyParser = require('body-parser'),
     path = require('path'),
     express = require('express'),
@@ -174,6 +175,68 @@ app.post('/playlist/:name', (req, res) => {
         });
     });
 });
+
+app.put('/playlist/swap/:id/:id2', (req, res) => {
+    let id = req.body.params.id;
+    let id2 = req.body.params.id2;
+
+    sequelize.transaction(transaction => {
+        return schema.playlist.findAll({
+            attributes: ["id", "position"],
+            where: {
+                $or: [{id}, {id: id2}]
+            },
+            transaction
+        }).then(playlists => {
+            if(playlists.length < 2) {
+                throw new Error();
+            }
+
+            let playlistId1 = playlists[0].id
+            let playlistPosition1 = playlists[0].position;
+
+            let playlistId2 = playlists[1].id
+            let playlistPosition2 = playlists[1].position;
+
+            return schema.playlist.update(
+                {position: -1},
+                {
+                    where: {
+                        id: playlistId1
+                    }
+                },
+                {transaction}
+            ).then((playlist) => {
+                return schema.playlist.update(
+                    {position: playlistPosition1},
+                    {
+                        where: {
+                            id: playlistId2
+                        }
+                    },
+                    {transaction}
+                ).then((playlist) => {
+                    return schema.playlist.update(
+                        {position: playlistPosition2},
+                        {
+                            where: {
+                                id: playlistId1
+                            }
+                        },
+                        {transaction}
+                    );
+                });
+            });
+        });
+    }).then(() => {
+        res.send();
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send("Invalid playlist Ids");
+    })
+
+
+})
 
 // rename playlist
 app.put('/playlist/:id/:newName', (req, res) => {
