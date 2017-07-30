8ec910460866beca124b23fef927b6db587f80bc
Tyler-R <trubenui@sfu.ca>
2017-07-29 17:44:56 -0700

    Add Dropdown to navbar settings tab

--- app/client/components/home/navbar/navbar.js
+++ app/client/components/home/navbar/navbar.js
@@ -1,21 +1,50 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import style from './navbar.scss';
+import dropdown from '../dropdown/dropdown.scss'
 
-const Navbar = () => {
-    return (
-        <div className="navbar-fixed">
-            <nav>
-                <div className="nav-wrapper white">
-                    <a href="#" className="navbar-logo">coursera-dl-viewer</a>
-                    <ul className="right">
-                        <li className="navbar-menu">Settings</li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    );
+class Navbar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    addCourseraCourse() {
+
+    }
+
+    addCourseraCourses() {
+
+    }
+
+    componentDidMount() {
+        $(document).ready(() => {
+            $(".navbar-menu").dropdown();
+        });
+    }
+
+    render() {
+        return (
+            <div className="navbar-fixed">
+                <ul id="navbar-dropdown" className="right navbar-dropdown-list">
+                    <li onClick={e => this.addCourseraCourse()}><a className="black-text">Add coursera-dl Course</a></li>
+                    <li onClick={e => this.addCourseraCourses()}><a className="black-text">Add coursera-dl Courses</a></li>
+                </ul>
+
+                <nav>
+                    <div className="nav-wrapper white">
+                        <a href="#" className="navbar-logo">coursera-dl-viewer</a>
+                        <span className="navbar-menu right" data-activates="navbar-dropdown">Settings</span>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default Navbar;
